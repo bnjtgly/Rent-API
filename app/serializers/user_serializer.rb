8016f0aa302a86579bde7dff62c56cfff4f539c5
Benjie Tigley <benjiebtigley@gmail.com>
2022-05-18 15:58:32 +0800

    RM-2500: Added user details in Chat API.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,10 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :complete_name
+  attributes :id, :email, :complete_name, :avatar
+
+  attribute :role do |user|
+    user.user_role.role.role_name
+  end
 
   attribute :chatrooms do |user|
     user.chatrooms.uniq
