2fe93162155de73b6e28be9ee98ff4fd1e809910
Benjie Tigley <benjiebtigley@gmail.com>
2022-06-23 09:41:19 +0800

    RM-2553: Fixed for mobile_number if null and response fixes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,12 +51,7 @@ class User < ApplicationRecord
   end
 
   def mobile_number
-    if !mobile_country_code_id.nil?
-      "+#{ref_mobile_country_code.value_str}#{mobile}"
-    else
-      nil
-    end
-    # "#{mobile_country_code_id.nil? ? '' : '+'}#{ref_mobile_country_code.value_str}#{mobile}"
+    !mobile_country_code_id.nil? ? "+#{ref_mobile_country_code.value_str}#{mobile}" : nil
   end
 
   def date_of_birth_format
